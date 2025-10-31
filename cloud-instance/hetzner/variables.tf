24c0f872a730c264377ad41e47ff4af5991b4182
Oleg Parashchenko <olpa@uucode.com>
2025-10-31 05:39:44 +0100

    Move specific value from the template.

--- cloud-instance/hetzner/variables.tf
+++ cloud-instance/hetzner/variables.tf
@@ -52,7 +52,7 @@ variable "ssh_key_name" {
 variable "ssh_public_key" {
   description = "SSH public key for server access"
   type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCtK9PUs5FdI8RJZjPLA8P7/YNHQJo3cySYI4QEzVQi+VVgFA/iaeM7XE3Y6al0+gS+cKDK2TgCM1fZZo1RsT2A6hO3IjMLswMwvOljPf4oHNlvgvK2Uiz4axJUgz21hHCbGMeea5FAZHwMfYhoN/LX4i1VQdfTBMSHC6+Co1rEMxhEPN9vjOkcvPe+VQ70BM7lbDA71CqESlGnXyw1TPjp0F2gX+BqZDWyYTfNkawWanxmdq4Pfc6R4bmimJsfDkniIPWZBCpx0m8R9UelA20oc+em8EGGniSTE9Wflj4WkW1VUr4bVMdfx6+MCowri8VWYeO918JfkuLeIAMSgAXsf7O3YVhq6x8teWi7+LmCtJ71vmJI+1yAKdVYKkOX46eBvgz7b+2cWpwhJlILjTZSENQfUl7iI2Du/E4tWSLHMBbWdE7OFS8AMFTCH8wUEOmtelMFbJfK/wcU7vsfyRjkp7fxpQ4eHets6YEs43b/sGYDRIxh6o/s9iOFjpXnzDs= olpa@office"
+  default     = "sh-rsa AAAAB3NzaC1yc2E... your-public-key-here"
 }
 
 variable "allowed_ssh_ips" {
