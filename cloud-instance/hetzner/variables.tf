b322e923b641f33c5e6645c13568596d430c9744
Oleg Parashchenko <olpa@uucode.com>
2025-10-31 05:18:37 +0100

    Tune.

--- cloud-instance/hetzner/variables.tf
+++ cloud-instance/hetzner/variables.tf
@@ -5,7 +5,7 @@ variable "hcloud_token" {
 }
 
 variable "server_name" {
-  description = "Name of the server"
+  description = "cloud works"
   type        = string
   default     = "temp-instance"
 }
@@ -13,7 +13,8 @@ variable "server_name" {
 variable "server_type" {
   description = "Server type (CX22 is cheapest shared vCPU)"
   type        = string
-  default     = "cx22"  # 2 vCPU, 4 GB RAM, ~€5.83/month
+  # default     = "cx22"  # 2 vCPU, 4 GB RAM, ~€5.83/month
+  default     = "cx23"  # 2 vCPU, 4 GB RAM, ~€5.83/month
   # For ARM: cax11 (2 vCPU ARM, 4 GB RAM, ~€4.45/month) - cheapest option
 }
 
@@ -31,7 +32,7 @@ variable "location" {
 }
 
 variable "volume_name" {
-  description = "Name of the cloud volume"
+  description = "cloud works"
   type        = string
   default     = "attachable-storage"
 }
@@ -43,7 +44,7 @@ variable "volume_size" {
 }
 
 variable "ssh_key_name" {
-  description = "Name for the SSH key"
+  description = "cloud works"
   type        = string
   default     = "terraform-key"
 }
@@ -51,6 +52,7 @@ variable "ssh_key_name" {
 variable "ssh_public_key" {
   description = "SSH public key for server access"
   type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCtK9PUs5FdI8RJZjPLA8P7/YNHQJo3cySYI4QEzVQi+VVgFA/iaeM7XE3Y6al0+gS+cKDK2TgCM1fZZo1RsT2A6hO3IjMLswMwvOljPf4oHNlvgvK2Uiz4axJUgz21hHCbGMeea5FAZHwMfYhoN/LX4i1VQdfTBMSHC6+Co1rEMxhEPN9vjOkcvPe+VQ70BM7lbDA71CqESlGnXyw1TPjp0F2gX+BqZDWyYTfNkawWanxmdq4Pfc6R4bmimJsfDkniIPWZBCpx0m8R9UelA20oc+em8EGGniSTE9Wflj4WkW1VUr4bVMdfx6+MCowri8VWYeO918JfkuLeIAMSgAXsf7O3YVhq6x8teWi7+LmCtJ71vmJI+1yAKdVYKkOX46eBvgz7b+2cWpwhJlILjTZSENQfUl7iI2Du/E4tWSLHMBbWdE7OFS8AMFTCH8wUEOmtelMFbJfK/wcU7vsfyRjkp7fxpQ4eHets6YEs43b/sGYDRIxh6o/s9iOFjpXnzDs= olpa@office"
 }
 
 variable "allowed_ssh_ips" {
